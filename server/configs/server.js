b586f915ffd17af153ed5daa1deeff7bb56de6c2
Mahmoud Nagy <mahmoud.nagyabosrea@gmail.com>
2024-03-23 14:10:18 +0200

    delete .env + force to connect to 5001 PORT

--- server/configs/server.js
+++ server/configs/server.js
@@ -3,8 +3,12 @@ const http = require('http');
 const server = http.createServer(app);
 
 
-server.listen(process.env.PORT || 4000, () => {
-    console.log(`Server is up and runing on port ${process.env.PORT}!`)
+// server.listen(process.env.PORT || 4000, () => {
+//     console.log(`Server is up and runing on port ${process.env.PORT}!`)
+// })
+
+server.listen(5001, () => {
+    console.log(`Server is up and runing on port 5001`)
 })
 
 
