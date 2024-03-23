02ec95540c6e4768c9e6629a153f7a4b185a57f1
Mahmoud Nagy <mahmoud.nagyabosrea@gmail.com>
2024-03-23 12:59:05 +0200

    database uri to be a string

--- server/configs/database.js
+++ server/configs/database.js
@@ -1,13 +1,14 @@
-const mongoose = require("mongoose")
+// const mongoose = require("mongoose")
 
-const uriMap = {
-    local: process.env.LOCAL_DB_URI,
-    development: process.env.DEV_DB_URL
-};
-const selectedEnv = process.env.CURRENT_URL || 'development';
-let uri = uriMap[selectedEnv];
+// const uriMap = {
+//     local: process.env.LOCAL_DB_URI,
+//     development: process.env.DEV_DB_URL
+// };
+// const selectedEnv = process.env.CURRENT_URL || 'development';
+// let uri = uriMap[selectedEnv];
+
 const connection = async () => {
-    return mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true })
+    return mongoose.connect("mongodb+srv://robosealerdev:3P6zyaEuiRhCnT6m@robosealerdev.eb1vyhp.mongodb.net/?retryWrites=true&w=majority", { useNewUrlParser: true, useUnifiedTopology: true })
         .then(() => {
             console.log(`Connected to MongoDB database successfully on ${selectedEnv} environment!`);
 
