3053fe4b74e27c67c68c439377aa2ef85f2090e7
Mahmoud Nagy <mahmoud.nagyabosrea@gmail.com>
2024-04-17 15:46:51 +0200

    use axios instead of direct url for image

--- client/src/rosbridge/ImageStream.tsx
+++ client/src/rosbridge/ImageStream.tsx
@@ -1,10 +1,40 @@
+import { useState, useEffect } from "react";
+import axios from "axios";
+
 const ImageStream = () => {
+  const [imageSrc, setImageSrc] = useState("");
+
+  useEffect(() => {
+    const fetchImageStream = async () => {
+      try {
+        const response = await axios.get(
+          "http://abdxi:8080/stream?topic=/image_raw",
+          {
+            responseType: "blob", // Set responseType to 'blob' to handle image data
+          }
+        );
+        // Convert image data to a data URL
+        const imageUrl = URL.createObjectURL(response.data);
+        setImageSrc(imageUrl);
+      } catch (error) {
+        console.error("Error fetching image stream:", error);
+      }
+    };
+
+    fetchImageStream();
+
+    // Cleanup function to revoke the URL object when component unmounts
+    return () => {
+      URL.revokeObjectURL(imageSrc);
+    };
+  }, []);
+
   return (
     <>
       <strong>Image Stream</strong>
 
       <img
-        src="http://abdxi:8080/stream?topic=/image_raw"
+        src={imageSrc}
         style={{ width: "100%", height: "100%", objectFit: "contain" }}
         alt="Image Stream"
       />
